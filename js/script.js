7fd3bd7be142faf5d8837d976f8ea29f7d0e948c
William Trevisan de Almeida <37343707+wtalmeida@users.noreply.github.com>
2020-06-23 14:05:34 -0300

    Batch update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
 
+
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-170464751-1');
+    
 /* slider buttons */
 $(function() {
     try {
